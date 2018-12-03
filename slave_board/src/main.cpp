cee072180a22742481926c3070c82fd90c40d5dd
Paul Schneider <pschn002@ucr.edu>
2018-12-02 16:52:53 -0800

    After Merge initial

--- slave_board/src/main.cpp
+++ slave_board/src/main.cpp
@@ -9,6 +9,7 @@
 #define EMERGENCY 0x01
 #define NORTH_SOUTH 0x02
 #define EAST_WEST 0x04
+#define ERROR_MODE 0x80
 
 #define MY_ADDRESS 8
 
@@ -32,37 +33,94 @@ void receiveEvent(int var) {
 
 	unsigned char rx_slave_id = Wire.read();
 	unsigned char bulbs = Wire.read();
-	unsigned char tmp_status_reg = Wire.read();
+	//unsigned char tmp_status_reg = Wire.read();
 	unsigned int tmp_time_in_pattern = 0;
 	for (int i = 0; i < 4; i++) {
 		unsigned char c = Wire.read();
 		tmp_time_in_pattern |= (c << (8 * i));
 	}
-	Serial.println(millis());
-	Serial.print(" - - - - - - -");
+	
+	Serial.println("- - - - - - - - - -");
 	//Serial.println(Wire.available());
+	Serial.println(millis());
+	Serial.print("\tSlave ID: ");
 	Serial.println(rx_slave_id);
-	Serial.println(bulbs);
-	Serial.println(tmp_status_reg);
-
+	Serial.print("\tBulb Output: ");
+	Serial.println(bulbs, HEX);
+	Serial.print("\tStatus: ");
+	Serial.println(status_reg, HEX);
+	Serial.print("\tTIP: ");
 	Serial.println(tmp_time_in_pattern);
 	Serial.println("- - - - - - - - - -");
 }
 
+void requestEvent() {
+ 
+    Wire.write(status_reg);
+    status_reg = status_reg & 0x00;
+}
+
+
 void emergencyMode() {
 
 	Serial.println("entering emergency mode");
+	status_reg = status_reg | ERROR_MODE;
 }
+
+void Button_Push()
+{
+	status_reg = status_reg | NORTH_SOUTH;
+    Serial.println("0x03");  
+   
+}
+
+void Button_Push2()
+{
+    
+	status_reg = status_reg | EAST_WEST;
+    Serial.println("0x02");  
+    
+}
+
+void IR_Sensor()
+{
+  val = analogRead(0);
+
+  if(val < 970 && EmergencyFlag == 0)
+  {
+     status_reg = status_reg | EMERGENCY;
+     Serial.println("0x01");
+     EmergencyFlag = 1;
+  }
+  else if(EmergencyFlag == 1 and val > 970)
+  {
+    EmergencyFlag = 0;
+  }
+  
+}
+
 
 void setup() {
 	Serial.begin(38400);
 	Wire.begin(MY_ADDRESS);
 	Wire.onReceive(*receiveEvent);
+ 	Wire.onRequest(requestEvent);
+	pinMode(ButtonPin, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(ButtonPin), Button_Push, RISING);
+	pinMode(ButtonPin2, INPUT_PULLUP);
+	attachInterrupt(digitalPinToInterrupt(ButtonPin2), Button_Push2, RISING);
+	Serial.println("startup");
+
+	DDRB = 0xFF;
+  	PORTB = 0;
+
+
 }
 
 void loop() {
 	// put your main code here, to run repeatedly:
 	delay(100);
+	IR_Sensor();
 	last_instruction_delay++;
 
 	 // TODO
